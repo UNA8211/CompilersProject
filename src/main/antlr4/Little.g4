649c682612f47245a4b8321b219d2860b541921c
Ryan Brand <UNA8949@gmail.com>
2018-02-02 16:55:19 -0700

    Working, complete code for step 1

--- src/main/antlr4/Little.g4
+++ src/main/antlr4/Little.g4
@@ -4,7 +4,7 @@ lexer grammar Little;
 // So, please keep the first line as it is provided.
 // Please write your grammar below this line.
 
-KEYWORD :'PROGRAM' | 'STRING';//'PROGRAM'|'BEGIN'|'END'|'FUNCTION'|'READ'|'WRITE'|'IF'|'ELSE'|'FI'|'FOR'|'ROF'|'RETURN'|'INT'|'VOID'|'STRING'|'FLOAT'|'WHILE'|'ENDIF'|'ENDWHILE';
+KEYWORD :'PROGRAM'|'STRING'|'PROGRAM'|'BEGIN'|'END'|'FUNCTION'|'READ'|'WRITE'|'IF'|'ELSE'|'FI'|'FOR'|'ROF'|'RETURN'|'INT'|'VOID'|'STRING'|'FLOAT'|'WHILE'|'ENDIF'|'ENDWHILE';
 
 WHITESPACE : ' ' -> skip;
 
@@ -12,14 +12,16 @@ NEWLINE : '\n' -> skip;
 
 RETURN : '\r' -> skip;
 
+TAB : '\t' -> skip;
+
 IDENTIFIER : [A-z][A-z0-9]*;
-//
-//INTLITERAL : [0-9]+;
-//
-//FLOATLITERAL : [0-9]*\\.[0-9]+;
-//
+
+INTLITERAL : [0-9]+;
+
+FLOATLITERAL : [0-9]*'.'[0-9]+;
+
 STRINGLITERAL : '"'.*?'"';
-//
-//COMMENT : --.*;
-//
-//OPERATOR : :=|\\+|-|\\*|\\/|=|!=|<|>|\\(|\\)|;|,|<=|>=;
+
+COMMENT : '--'.*?'\n' -> skip;
+
+OPERATOR : ':='|'+'|'-'|'*'|'/'|'='|'!='|'<'|'>'|'('|')'|';'|','|'<='|'>=';
--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,33 +1,27 @@
 import org.antlr.v4.runtime.*;
-import org.antlr.v4.runtime.tree.ParseTree;
-import org.antlr.v4.runtime.tree.ParseTreeWalker;
-
-import java.util.ArrayList;
 
 public class Driver {
     public static void main (String[] args) throws Exception{
         String fileName = "C:\\Users\\Ryan Brand\\Documents\\S2018\\CSCI468\\CompilerProject\\src\\main\\java\\parse_file.txt";
-        CharStream input = new ANTLRFileStream(fileName);
+        CharStream input = CharStreams.fromFileName(fileName);
 
         Little littleLexer = new Little(input);
         CommonTokenStream commonTokenStream = new CommonTokenStream(littleLexer);
         commonTokenStream.fill();
-        for (String name:
-             littleLexer.getRuleNames()) {
-            System.out.println(name);
-        }
-
-        System.out.println();
 
         // This loop implementation for extracting rule and value adapted from
         // https://www.programcreek.com/java-api-examples/index.php?api=org.antlr.v4.runtime.CommonTokenStream
+        String[] tokenNames = littleLexer.getRuleNames();
         for (Token token : commonTokenStream.getTokens()) {
             CommonToken ct = (CommonToken) token;
-            String[] tokenNames = littleLexer.getTokenNames();
-            int type = ct.getType();
-            String tokenName = type > 0 ? tokenNames[type] : "EOF";
-            System.out.println("LEXER: " + tokenName);
-            System.out.println("VALUE: " + ct.getText());
+            int type = ct.getType() - 1;
+            //EOF
+            if (type < 0) {
+                break;
+            }
+            String tokenName = tokenNames[type];
+            System.out.println("Token Type: " + tokenName);
+            System.out.println("Value: " + ct.getText());
         }
     }
 }
