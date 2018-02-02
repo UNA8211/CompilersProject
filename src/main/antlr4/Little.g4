1aa664a276b3cc095a8ee68b85211af142317643
Ryan Brand <UNA8949@gmail.com>
2018-02-02 12:04:00 -0700

    Added source files

---
+++ src/main/antlr4/Little.g4
@@ -0,0 +1,25 @@
+lexer grammar Little;
+
+// First line of this file must be the grammar name.
+// So, please keep the first line as it is provided.
+// Please write your grammar below this line.
+
+KEYWORD :'PROGRAM' | 'STRING';//'PROGRAM'|'BEGIN'|'END'|'FUNCTION'|'READ'|'WRITE'|'IF'|'ELSE'|'FI'|'FOR'|'ROF'|'RETURN'|'INT'|'VOID'|'STRING'|'FLOAT'|'WHILE'|'ENDIF'|'ENDWHILE';
+
+WHITESPACE : ' ' -> skip;
+
+NEWLINE : '\n' -> skip;
+
+RETURN : '\r' -> skip;
+
+IDENTIFIER : [A-z][A-z0-9]*;
+//
+//INTLITERAL : [0-9]+;
+//
+//FLOATLITERAL : [0-9]*\\.[0-9]+;
+//
+STRINGLITERAL : '"'.*?'"';
+//
+//COMMENT : --.*;
+//
+//OPERATOR : :=|\\+|-|\\*|\\/|=|!=|<|>|\\(|\\)|;|,|<=|>=;
---
+++ src/main/java/Driver.java
@@ -0,0 +1,32 @@
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+
+import java.util.ArrayList;
+
+public class Driver {
+    public static void main (String[] args) throws Exception{
+        CharStream input = new ANTLRFileStream("C:\\Users\\Ryan Brand\\Documents\\S2018\\CSCI468\\CompilerProject\\src\\main\\java\\parse_file.txt");
+
+        Little littleLexer = new Little(input);
+        CommonTokenStream commonTokenStream = new CommonTokenStream(littleLexer);
+        commonTokenStream.fill();
+        for (String name:
+             littleLexer.getRuleNames()) {
+            System.out.println(name);
+        }
+
+        System.out.println();
+
+        // This loop implementation for extracting rule and value adapted from
+        // https://www.programcreek.com/java-api-examples/index.php?api=org.antlr.v4.runtime.CommonTokenStream
+        for (Token token : commonTokenStream.getTokens()) {
+            CommonToken ct = (CommonToken) token;
+            String[] tokenNames = littleLexer.getTokenNames();
+            int type = ct.getType();
+            String tokenName = type > 0 ? tokenNames[type] : "EOF";
+            System.out.println("LEXER: " + tokenName);
+            System.out.println("VALUE: " + ct.getText());
+        }
+    }
+}
