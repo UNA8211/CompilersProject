65470edbe474df3d648e2e96447c21f5359942f6
Ryan Brand <UNA8949@gmail.com>
2018-02-05 19:56:04 -0700

    Separated lexer from driver class

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -4,24 +4,8 @@ public class Driver {
     public static void main (String[] args) throws Exception{
         String fileName = "C:\\Users\\Ryan Brand\\Documents\\S2018\\CSCI468\\CompilerProject\\src\\main\\java\\parse_file.txt";
         CharStream input = CharStreams.fromFileName(fileName);
-
-        Little littleLexer = new Little(input);
-        CommonTokenStream commonTokenStream = new CommonTokenStream(littleLexer);
-        commonTokenStream.fill();
-
-        // This loop implementation for extracting rule and value adapted from
-        // https://www.programcreek.com/java-api-examples/index.php?api=org.antlr.v4.runtime.CommonTokenStream
-        String[] tokenNames = littleLexer.getRuleNames();
-        for (Token token : commonTokenStream.getTokens()) {
-            CommonToken ct = (CommonToken) token;
-            int type = ct.getType() - 1;
-            //EOF
-            if (type < 0) {
-                break;
-            }
-            String tokenName = tokenNames[type];
-            System.out.println("Token Type: " + tokenName);
-            System.out.println("Value: " + ct.getText());
-        }
+        LexerService lexerService = new LexerService(input);
+        lexerService.fillStream();
+        lexerService.printTokens();
     }
 }
---
+++ src/main/java/LexerService.java
@@ -0,0 +1,41 @@
+import org.antlr.v4.runtime.*;
+
+public class LexerService {
+
+    private Lexer littleLexer = null;
+    private CommonTokenStream commonTokenStream = null;
+
+    public LexerService(CharStream input) {
+        littleLexer = new Little(input);
+        commonTokenStream = new CommonTokenStream(littleLexer);
+    }
+
+    public void fillStream() {
+        commonTokenStream.fill();
+    }
+
+    public void printTokens() {
+        // This loop implementation for extracting rule and value adapted from
+        // https://www.programcreek.com/java-api-examples/index.php?api=org.antlr.v4.runtime.CommonTokenStream
+        String[] tokenNames = littleLexer.getRuleNames();
+        for (Token token : commonTokenStream.getTokens()) {
+            CommonToken ct = (CommonToken) token;
+            int type = ct.getType() - 1;
+            //EOF
+            if (type < 0) {
+                break;
+            }
+            String tokenName = tokenNames[type];
+            System.out.println("Token Type: " + tokenName);
+            System.out.println("Value: " + ct.getText());
+        }
+    }
+
+    public Lexer getLittleLexer() {
+        return littleLexer;
+    }
+
+    public CommonTokenStream getCommonTokenStream() {
+        return commonTokenStream;
+    }
+}
